7494877bd129d2d04b38d93a5095360ba2736052
xanxustan <xiaofei.tan@intellectia.cn>
2023-12-14 15:12:23 +0800

    add meeting type

--- protobufs/audio/v1/audio.pb.gw.go
+++ protobufs/audio/v1/audio.pb.gw.go
@@ -415,6 +415,24 @@ func local_request_AudioService_GetSchedule_0(ctx context.Context, marshaler run
 
 }
 
+func request_AudioService_GetMeetingType_0(ctx context.Context, marshaler runtime.Marshaler, client AudioServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq GetMeetingTypeReq
+	var metadata runtime.ServerMetadata
+
+	msg, err := client.GetMeetingType(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_AudioService_GetMeetingType_0(ctx context.Context, marshaler runtime.Marshaler, server AudioServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq GetMeetingTypeReq
+	var metadata runtime.ServerMetadata
+
+	msg, err := server.GetMeetingType(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
 // RegisterAudioServiceHandlerServer registers the http handlers for service AudioService to "mux".
 // UnaryRPC     :call AudioServiceServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
@@ -693,6 +711,31 @@ func RegisterAudioServiceHandlerServer(ctx context.Context, mux *runtime.ServeMu
 		}
 
 		forward_AudioService_GetSchedule_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
+	mux.Handle("GET", pattern_AudioService_GetMeetingType_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		var err error
+		var annotatedContext context.Context
+		annotatedContext, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/chatfinbot.audio.v1.AudioService/GetMeetingType", runtime.WithHTTPPathPattern("/v1/audio/meeting-type"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_AudioService_GetMeetingType_0(annotatedContext, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_AudioService_GetMeetingType_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -979,6 +1022,28 @@ func RegisterAudioServiceHandlerClient(ctx context.Context, mux *runtime.ServeMu
 
 	})
 
+	mux.Handle("GET", pattern_AudioService_GetMeetingType_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		var err error
+		var annotatedContext context.Context
+		annotatedContext, err = runtime.AnnotateContext(ctx, mux, req, "/chatfinbot.audio.v1.AudioService/GetMeetingType", runtime.WithHTTPPathPattern("/v1/audio/meeting-type"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_AudioService_GetMeetingType_0(annotatedContext, inboundMarshaler, client, req, pathParams)
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_AudioService_GetMeetingType_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -1004,6 +1069,8 @@ var (
 	pattern_AudioService_UpdateRecord_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "audio", "update-record"}, ""))
 
 	pattern_AudioService_GetSchedule_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "audio", "schedule"}, ""))
+
+	pattern_AudioService_GetMeetingType_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "audio", "meeting-type"}, ""))
 )
 
 var (
@@ -1028,4 +1095,6 @@ var (
 	forward_AudioService_UpdateRecord_0 = runtime.ForwardResponseMessage
 
 	forward_AudioService_GetSchedule_0 = runtime.ForwardResponseMessage
+
+	forward_AudioService_GetMeetingType_0 = runtime.ForwardResponseMessage
 )
