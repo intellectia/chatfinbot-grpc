4b592e4e89e9abb2378c120c224268cb9ddf69d0
guxueying <xueying.gu@everxyz.onaliyun.com>
2023-12-15 14:38:36 +0800

    [rpc]: remove old rpc

--- protobufs/payment/v1/payment.pb.gw.go
+++ protobufs/payment/v1/payment.pb.gw.go
@@ -190,42 +190,6 @@ func local_request_PaymentsService_GetPlanDetail_0(ctx context.Context, marshale
 }
 
 var (
-	filter_PaymentsService_GetUserUsagePermissions_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
-)
-
-func request_PaymentsService_GetUserUsagePermissions_0(ctx context.Context, marshaler runtime.Marshaler, client PaymentsServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq GetUserUsagePermissionsRequest
-	var metadata runtime.ServerMetadata
-
-	if err := req.ParseForm(); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_PaymentsService_GetUserUsagePermissions_0); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
-
-	msg, err := client.GetUserUsagePermissions(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
-	return msg, metadata, err
-
-}
-
-func local_request_PaymentsService_GetUserUsagePermissions_0(ctx context.Context, marshaler runtime.Marshaler, server PaymentsServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq GetUserUsagePermissionsRequest
-	var metadata runtime.ServerMetadata
-
-	if err := req.ParseForm(); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_PaymentsService_GetUserUsagePermissions_0); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
-	}
-
-	msg, err := server.GetUserUsagePermissions(ctx, &protoReq)
-	return msg, metadata, err
-
-}
-
-var (
 	filter_PaymentsService_GetUserUsage_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
 )
 
@@ -398,31 +362,6 @@ func RegisterPaymentsServiceHandlerServer(ctx context.Context, mux *runtime.Serv
 		}
 
 		forward_PaymentsService_GetPlanDetail_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
-
-	})
-
-	mux.Handle("GET", pattern_PaymentsService_GetUserUsagePermissions_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
-		ctx, cancel := context.WithCancel(req.Context())
-		defer cancel()
-		var stream runtime.ServerTransportStream
-		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
-		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		var err error
-		var annotatedContext context.Context
-		annotatedContext, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/chatfinbot.payment.v1.PaymentsService/GetUserUsagePermissions", runtime.WithHTTPPathPattern("/usage-details"))
-		if err != nil {
-			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
-			return
-		}
-		resp, md, err := local_request_PaymentsService_GetUserUsagePermissions_0(annotatedContext, inboundMarshaler, server, req, pathParams)
-		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
-		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
-		if err != nil {
-			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
-			return
-		}
-
-		forward_PaymentsService_GetUserUsagePermissions_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -605,28 +544,6 @@ func RegisterPaymentsServiceHandlerClient(ctx context.Context, mux *runtime.Serv
 
 	})
 
-	mux.Handle("GET", pattern_PaymentsService_GetUserUsagePermissions_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
-		ctx, cancel := context.WithCancel(req.Context())
-		defer cancel()
-		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		var err error
-		var annotatedContext context.Context
-		annotatedContext, err = runtime.AnnotateContext(ctx, mux, req, "/chatfinbot.payment.v1.PaymentsService/GetUserUsagePermissions", runtime.WithHTTPPathPattern("/usage-details"))
-		if err != nil {
-			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
-			return
-		}
-		resp, md, err := request_PaymentsService_GetUserUsagePermissions_0(annotatedContext, inboundMarshaler, client, req, pathParams)
-		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
-		if err != nil {
-			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
-			return
-		}
-
-		forward_PaymentsService_GetUserUsagePermissions_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
-
-	})
-
 	mux.Handle("GET", pattern_PaymentsService_GetUserUsage_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -683,8 +600,6 @@ var (
 
 	pattern_PaymentsService_GetPlanDetail_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0}, []string{"plan-details"}, ""))
 
-	pattern_PaymentsService_GetUserUsagePermissions_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0}, []string{"usage-details"}, ""))
-
 	pattern_PaymentsService_GetUserUsage_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0}, []string{"usage"}, ""))
 
 	pattern_PaymentsService_ConsumeUserPackageUsage_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0}, []string{"consume-package-usage"}, ""))
@@ -698,8 +613,6 @@ var (
 	forward_PaymentsService_GetOrderStatus_0 = runtime.ForwardResponseMessage
 
 	forward_PaymentsService_GetPlanDetail_0 = runtime.ForwardResponseMessage
-
-	forward_PaymentsService_GetUserUsagePermissions_0 = runtime.ForwardResponseMessage
 
 	forward_PaymentsService_GetUserUsage_0 = runtime.ForwardResponseMessage
 
